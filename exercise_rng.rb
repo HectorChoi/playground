eb1e758eb98540a7b522ed257b3a262c5a8b15b0
Hector Choi <hctrchoi@gmail.com>
2021-10-24 10:27:15 -0700

    formatted indentation

--- exercise_rng.rb
+++ exercise_rng.rb
@@ -1,17 +1,17 @@
 exercises = {
-  reading_documentation: 8,
+  reading_documentation:  8,
   reading_documentation2: 3,
-  variable_scope: 10,
-  loops1: 10,
-  loops2: 10,
-  user_input: 10,
-  methods: 10,
-  return: 10,
-  conditionals: 10,
-  strings: 10,
-  arrays: 10,
-  hashes: 10,
-  debugging: 10
+  variable_scope:         10,
+  loops1:                 10,
+  loops2:                 10,
+  user_input:             10,
+  methods:                10,
+  return:                 10,
+  conditionals:           10,
+  strings:                10,
+  arrays:                 10,
+  hashes:                 10,
+  debugging:              10
 }
 
 exercise_topic = exercises.keys.sample
